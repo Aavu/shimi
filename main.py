a158dfdb58d9845df24a8aa532c8a496f707b0eb
aavu <aavu@gatech.edu>
2024-04-15 12:55:27 -0400

    cleaned up main.py

--- main.py
+++ main.py
@@ -2,18 +2,7 @@
 Author: Raghavasimhan Sankaranarayanan
 Date created: 03/03/24
 """
-
-import time
-
-import numpy as np
-
 from performance import Performance
-from song import Song
-from definitions import *
-from shimi import Shimi, Command
-
-robot = Shimi(LIMITS)
-
 
 if __name__ == '__main__':
     p = Performance(song_library_path="songs", gesture_library_path="gestures", chunk_size=1024, sample_rate=48000)
